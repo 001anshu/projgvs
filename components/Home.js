4b6079066b273a6c81c44bc0d1e0e641313b773f
ANSHU KUMAR SAW <anshusaw7563@gmail.com>
2024-06-08 17:14:34 +0530

    wraped it up

--- components/Home.js
+++ components/Home.js
@@ -14,10 +14,10 @@ const Home = () => {
         <p className="text-md m-4 lg:m-10">
           The Society for Nature and Humanity is a dedicated organization inspired by Vedic India and the teachings of the Bhagavad Gita, committed to fostering a harmonious relationship between individuals, communities, and the environment. Our mission encompasses holistic service through initiatives like goshala (cow sanctuary), gurukuls (traditional schools), and youth programs that guide and educate based on spiritual and ethical values. We conduct seminars and workshops on the Bhagavad Gita to impart wisdom and promote sustainable living. Engaging in environmental initiatives, our society aims to create a balanced ecosystem where human potential is realized in harmony with nature, providing a sanctuary of learning, growth, and community service.
         </p>
-        <Link href="/construction">
+        {/* <Link href="/construction">
                   <button className="bg-indigo-500 p-4 m-4 text-white">ENROLL NOW →</button>
 
-        </Link>
+        </Link> */}
       </div>
 
       <div className="md:flex h-auto relative overflow-hidden">
