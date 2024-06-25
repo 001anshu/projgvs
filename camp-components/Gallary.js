9d232a61178c759135f7b9910e6ed6ee4a455ffc
ANSHU KUMAR SAW <anshusaw7563@gmail.com>
2024-06-25 17:19:23 +0530

    consistent padding

--- camp-components/Gallary.js
+++ camp-components/Gallary.js
@@ -21,11 +21,26 @@ const Gallary = () => {
       src: "https://lh3.googleusercontent.com/u/0/drive-viewer/AKGpihbh4A-QtC1iFuw39NKo-2G-gbHEbhtiAK2dqTcX_zAOqQBYvoH8mcQVLnxg4LLFI5Cp6t6wfO5hVh7A1zIiXDasrGaPSsg6OWI=w1160-h878-rw-v1",
       alt: "camp02",
     },
-    { src: "https://lh3.googleusercontent.com/u/0/drive-viewer/AKGpihbXz97UgHPO1MGQ4p-hdKaQfRKFNXijVGczl1538FAviWGfcoJCOsfyiEtLof9pfnU6ryLgIvuWjmsrSAC0bNRXugG_9n8hZog=w1160-h878", alt: "camp03" },
-    { src: "https://lh3.googleusercontent.com/u/0/drive-viewer/AKGpihZ716Lm47psFdJgx0rYj3HNf5V05fP8W_qrFVZpsRj5yCKmuXCYk2Pe26y9je8j9xF1uligeieJbgTOgapkV0bEIbNd8tSNSw=w1160-h878", alt: "camp04" },
-    { src: "https://lh3.googleusercontent.com/u/0/drive-viewer/AKGpihZYYdRsCsjeNayeltZyAOnY1u4H6fAo61UY6BwZFQbuFmqergz8TrqBqdF_26vs9fNp5CKH3Y9of4w9gZ82ZEj98zAOfdycrQ=w1160-h878", alt: "camp05" },
-    { src: "https://lh3.googleusercontent.com/u/0/drive-viewer/AKGpihY3yCuxTFxEqiKW-kkszfnOCyicOJA-T9gx87EtE4q2qhxbVrcGNUAZwU4HI7nnzP1KcEHJKTIz1x9mnK_i9ESzn33sg7-vldU=w1160-h878", alt: "camp06" },
-    { src: "https://lh3.googleusercontent.com/u/0/drive-viewer/AKGpihYcVtPxChiSpjskRpFNa4jTZ9UBKz5cV-5iCt9b77rRFRqnWxZ2fK2kO6d7ubvMxwcfdVQlGb5zgx9moR_deLWg5VHJP--vY2g=w1160-h878", alt: "camp07" },
+    {
+      src: "https://lh3.googleusercontent.com/u/0/drive-viewer/AKGpihbXz97UgHPO1MGQ4p-hdKaQfRKFNXijVGczl1538FAviWGfcoJCOsfyiEtLof9pfnU6ryLgIvuWjmsrSAC0bNRXugG_9n8hZog=w1160-h878",
+      alt: "camp03",
+    },
+    {
+      src: "https://lh3.googleusercontent.com/u/0/drive-viewer/AKGpihZ716Lm47psFdJgx0rYj3HNf5V05fP8W_qrFVZpsRj5yCKmuXCYk2Pe26y9je8j9xF1uligeieJbgTOgapkV0bEIbNd8tSNSw=w1160-h878",
+      alt: "camp04",
+    },
+    {
+      src: "https://lh3.googleusercontent.com/u/0/drive-viewer/AKGpihZYYdRsCsjeNayeltZyAOnY1u4H6fAo61UY6BwZFQbuFmqergz8TrqBqdF_26vs9fNp5CKH3Y9of4w9gZ82ZEj98zAOfdycrQ=w1160-h878",
+      alt: "camp05",
+    },
+    {
+      src: "https://lh3.googleusercontent.com/u/0/drive-viewer/AKGpihY3yCuxTFxEqiKW-kkszfnOCyicOJA-T9gx87EtE4q2qhxbVrcGNUAZwU4HI7nnzP1KcEHJKTIz1x9mnK_i9ESzn33sg7-vldU=w1160-h878",
+      alt: "camp06",
+    },
+    {
+      src: "https://lh3.googleusercontent.com/u/0/drive-viewer/AKGpihYcVtPxChiSpjskRpFNa4jTZ9UBKz5cV-5iCt9b77rRFRqnWxZ2fK2kO6d7ubvMxwcfdVQlGb5zgx9moR_deLWg5VHJP--vY2g=w1160-h878",
+      alt: "camp07",
+    },
     // { src: "/camp08.webp", alt: "camp08" },
     // { src: "/camp09.webp", alt: "camp09" },
     // { src: "/camp10.jpg", alt: "camp10" },
@@ -56,43 +71,46 @@ const Gallary = () => {
   ];
 
   return (
-    <div
-      className="text-center max-w-3/5 "
-      style={{ backgroundColor: "rgb(27,37,46)", padding: "40px" }}
-      id="memories"
-    >
-      <div>
-        <p
-          className=""
-          style={{ fontSize: "3rem", color: "white", padding: "40px" }}
-        >
-          YATRA MEMORIES
-        </p>
-      </div>
+    <div className="flex justify-center">
+      {" "}
       <div
-        className=" scrollbar-hide scroll-smooth"
-        style={{
-          display: "flex",
-          flexWrap: "wrap",
-          justifyContent: "center",
-          maxHeight: "600px",
-          overflowY: "scroll",
-        }}
+        className="text-center w-3/4  "
+        style={{ backgroundColor: "rgb(27,37,46)", padding: "" }}
+        id="memories"
       >
-        {img.map((image, index) => (
-          <div key={index} style={{ width: "300px", padding: "8px" }}>
-             <img
-      src={image.src}
-      alt={image.alt}
-      width="300"
-      height="200"
-      className="object-cover w-full transition-transform duration-300 ease-in-out"
-      loading="lazy"
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-    />
-          </div>
-        ))}
+        <div>
+          <p
+            className=""
+            style={{ fontSize: "3rem", color: "white", padding: "40px" }}
+          >
+            YATRA MEMORIES
+          </p>
+        </div>
+        <div
+          className=" scrollbar-hide scroll-smooth"
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "center",
+            maxHeight: "600px",
+            overflowY: "scroll",
+          }}
+        >
+          {img.map((image, index) => (
+            <div key={index} style={{ width: "300px", padding: "8px" }}>
+              <img
+                src={image.src}
+                alt={image.alt}
+                width="300"
+                height="200"
+                className="object-cover w-full transition-transform duration-300 ease-in-out"
+                loading="lazy"
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
